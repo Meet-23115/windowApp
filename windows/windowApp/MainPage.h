e31779af4cef74a1cd485a5870b52dcdaddc3660
Meet Gelothra <imailmeet4000@gmail.com>
2025-01-07 14:19:39 +0530

    init

---
+++ windows/windowApp/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::windowApp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::windowApp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
